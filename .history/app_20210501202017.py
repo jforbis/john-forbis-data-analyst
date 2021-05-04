36a26f9a26502e620af5f36d0f6d318d0eb691d7
jforbis <forbisjohn@gmail.com>
2021-05-04 00:08:06 -0500

    .gitignore is now working

--- .history/app_20210501202017.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, redirect, jsonify, request
-
-app = Flask(__name__)
-
-@app.route("/", methods=['GET'])
-def home():
-    return render_template("index.html")
